e8faf4c2ac644c1d3754384e5494c405c9b90689
Jandan <lutao1096@gmail.com>
2020-09-04 03:28:58 +0800

    优化登入页

---
+++ client/src/components/common/social-login.vue
@@ -0,0 +1,77 @@
+<template>
+  <view>
+    <view class="flex align-center justify-center py-5">
+      <view style="width: 100rpx; height: 2rpx; background-color: #e3e1dd;"></view>
+      <view class="text-muted mx-2">社交账号登入</view>
+      <view style="width: 100rpx; height: 2rpx; background-color: #e3e1dd;"></view>
+    </view>
+    <view class="flex px-2 text-white">
+      <block v-for="(item, index) in providerList" :key="index">
+        <view v-if="item.icon" class="flex-fill flex align-center justify-center">
+          <view
+            class="rounded-circle flex align-center justify-center animate__animated"
+            :class="item.bgColor"
+            hover-class="animate__pulse"
+            style="width: 100rpx; height: 100rpx;"
+            @click.stop="onSocialLogin(item.id)"
+          >
+            <text class="uni-icon" :class="item.icon" style="font-size: 50rpx;"></text>
+          </view>
+        </view>
+      </block>
+    </view>
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      providerList: [],
+    }
+  },
+  created() {
+    uni.getProvider({
+      service: 'oauth',
+      success: (result) => {
+        this.providerList = result.provider.map((value) => {
+          let providerName = ''
+          let icon = ''
+          let bgColor = ''
+          switch (value) {
+            case 'weixin':
+              providerName = '微信登录'
+              icon = 'uni-icon-weixin'
+              bgColor = 'bg-success'
+              break
+            case 'qq':
+              providerName = 'QQ登录'
+              icon = 'uni-icon-qq'
+              bgColor = 'bg-primary'
+              break
+            case 'sinaweibo':
+              providerName = '新浪微博登录'
+              icon = 'uni-icon-weibo'
+              bgColor = 'bg-danger'
+              break
+          }
+          return {
+            name: providerName,
+            icon,
+            bgColor,
+            id: value,
+          }
+        })
+      },
+    })
+  },
+  methods: {
+    // 第三方登入
+    onSocialLogin(type) {
+      this.$emit('social', type)
+    },
+  },
+}
+</script>
+
+<style scoped></style>
--- client/src/pages/login/login.vue
+++ client/src/pages/login/login.vue
@@ -1,45 +1,88 @@
 <template>
   <view>
+    <uni-status-bar></uni-status-bar>
     <!-- 顶部关闭按钮 开始 -->
     <view
-      class="fixed-top left-0 flex align-center justify-center animate__animated"
+      class="flex align-center justify-center animate__animated"
       hover-class="text-main animate__pulse"
       style="width: 80rpx; height: 80rpx;"
-      :style="'top: ' + statusBarHeight + 'px;'"
       @click.stop="back()"
     >
       <text class="iconfont icon-guanbi font-lg"></text>
     </view>
     <!-- 顶部关闭按钮 结束 -->
 
-    <view class="flex align-center justify-center font-lg font-weight-bold" style="padding: 100rpx 0 80rpx;"
-      >欢迎回来</view
+    <view
+      class="text-center font-weight-bold"
+      style="font-size: 50rpx; padding: 80rpx 0 50rpx; letter-spacing: 10rpx;"
+      >{{ formType === 'login' ? (phoneLogin ? '手机登入' : '普通登入') : '注册' }}</view
     >
     <!-- 表单 开始 -->
     <view class="p-2">
+      <view class="flex align-center justify-center my-3">
+        <!-- 手机登入时才显示 -->
+        <text
+          v-if="formType === 'login' && phoneLogin"
+          class="flex align-center bg-light px-2 border-right"
+          style="height: 80rpx;"
+          >+86</text
+        >
+        <input
+          class="bg-light rounded px-2 flex-fill"
+          style="height: 80rpx;"
+          :placeholder="phoneLogin ? '手机号' : '用户名/邮箱'"
+          placeholder-class="text-light-muted font"
+          v-model="username"
+        />
+      </view>
+      <!-- 手机登入验证码框 -->
+      <view v-if="formType === 'login' && phoneLogin" class="flex align-center position-relative my-3">
+        <input
+          class="bg-light rounded px-2 flex-fill"
+          style="height: 80rpx;"
+          placeholder="请输入短信验证码"
+          placeholder-class="text-light-muted font"
+          :password="true"
+          v-model="smscode"
+        />
+        <view
+          class="position-absolute right-0 rounded-right flex align-center justify-center"
+          :class="smsTime > 0 ? 'bg-disabled-main' : 'bg-main'"
+          style="width: 160rpx; height: 80rpx;"
+          @click.stop="getSmscode"
+        >
+          <text class="text-white">{{ smsTime > 0 ? smsTime + 's' : '获取验证码' }}</text>
+        </view>
+      </view>
+
+      <!-- 普通登入密码框 -->
+      <view v-else class="flex align-center position-relative my-3">
+        <input
+          class="bg-light rounded px-2 flex-fill"
+          style="height: 80rpx;"
+          placeholder="请输入密码"
+          placeholder-class="text-light-muted font"
+          :password="true"
+          v-model="password"
+        />
+        <!-- 普通登入时才显示 -->
+        <text
+          v-if="formType === 'login' && !phoneLogin"
+          class="bg-light position-absolute right-0 text-light-muted font-sm"
+          style="letter-spacing: 4rpx;"
+          >忘记密码？</text
+        >
+      </view>
+
+      <!-- 注册时才显示 -->
       <input
-        class="bg-light rounded px-2 my-3"
-        style="height: 80rpx;"
-        placeholder="手机号/用户名/邮箱"
-        placeholder-class="text-light-muted font"
-        v-model="form.username"
-      />
-      <input
-        class="bg-light rounded px-2 my-3"
-        style="height: 80rpx;"
-        placeholder="请输入密码"
-        placeholder-class="text-light-muted font"
-        :password="true"
-        v-model="form.password"
-      />
-      <input
-        v-if="type === 'reg'"
+        v-if="formType === 'reg'"
         class="bg-light rounded px-2 my-3"
         style="height: 80rpx;"
         placeholder="请输入确认密码"
         placeholder-class="text-light-muted font"
         :password="true"
-        v-model="form.repassword"
+        v-model="repassword"
       />
     </view>
     <view class="px-4">
@@ -47,105 +90,116 @@
         type="primary"
         class="border-0 text-white"
         style="border-radius: 50rpx; letter-spacing: 6rpx;"
-        :class="disabled ? 'bg-disabled-main' : 'bg-main'"
-        :disabled="disabled"
+        :class="disabled || loading ? 'bg-disabled-main' : 'bg-main'"
+        :disabled="disabled || loading"
         @click.stop="submit()"
       >
-        {{ type === 'login' ? '登入' : '注册' }}
+        {{ formType === 'login' ? '登入' : '注册' }}
       </button>
     </view>
     <!-- 表单 结束 -->
 
     <!-- 切换表单 开始 -->
-    <view class="flex px-4 mt-4">
-      <text class="ml-auto px-2 text-light-muted" @click.stop="changeType">
-        {{ type === 'login' ? '还没账号？' : '已有账号，'
-        }}<text class="text-main">{{ type === 'login' ? '去注册' : '去登入' }}</text>
+    <view class="flex px-4 mt-4 text-light-muted">
+      <text v-if="formType === 'login'" class="flex-fill px-2 text-left" @click.stop="changeLoginType">{{
+        phoneLogin ? '普通登入' : '手机登入'
+      }}</text>
+      <text class="flex-fill px-2 text-right" @click.stop="changeType">
+        {{ formType === 'login' ? '还没账号？' : '已有账号，'
+        }}<text class="text-main">{{ formType === 'login' ? '去注册' : '去登入' }}</text>
       </text>
     </view>
     <!-- 切换表单 结束 -->
 
-    <!-- 设计登入按钮 开始 -->
-    <view class="flex align-center justify-center py-5">
-      <view style="width: 100rpx; height: 2rpx; background-color: #e3e1dd;"></view>
-      <view class="text-muted mx-2">社交账号登入</view>
-      <view style="width: 100rpx; height: 2rpx; background-color: #e3e1dd;"></view>
-    </view>
-    <view class="flex px-2 text-white">
-      <view class="flex-fill flex align-center justify-center">
-        <view
-          class="rounded-circle bg-success flex align-center justify-center animate__animated"
-          hover-class="animate__pulse"
-          style="width: 100rpx; height: 100rpx;"
-          @click="socialLogin('wechat')"
-        >
-          <text class="iconfont icon-weixin font-lg"></text>
-        </view>
-      </view>
-      <view class="flex-fill flex align-center justify-center">
-        <view
-          class="rounded-circle bg-primary flex align-center justify-center animate__animated"
-          hover-class="animate__pulse"
-          style="width: 100rpx; height: 100rpx;"
-          @click="socialLogin('qq')"
-        >
-          <text class="iconfont icon-QQ font-lg"></text>
-        </view>
-      </view>
-      <view class="flex-fill flex align-center justify-center">
-        <view
-          class="rounded-circle bg-danger flex align-center justify-center animate__animated"
-          hover-class="animate__pulse"
-          style="width: 100rpx; height: 100rpx;"
-          @click="socialLogin('sina')"
-        >
-          <text class="iconfont icon-xinlangweibo font-lg"></text>
-        </view>
-      </view>
-    </view>
-    <!-- 设计登入按钮 结束 -->
+    <!-- 社交登入按钮 开始 -->
+    <social-login @social="socialLogin"></social-login>
+    <!-- 社交登入按钮 结束 -->
   </view>
 </template>
 
 <script>
+const phoneRegex = /^1[345789]\d{9}$/
 import common from '@/common/mixins/common'
+import socialLogin from '@/components/common/social-login'
+import uniStatusBar from '@/components/uni-ui/uni-status-bar/uni-status-bar'
 export default {
-  components: {},
+  components: {
+    socialLogin,
+    uniStatusBar,
+  },
   mixins: [common],
   data() {
     return {
-      type: 'login',
-      statusBarHeight: 0,
-      form: { username: '', password: '', repassword: '' },
+      loading: false,
+      formType: 'login',
+      phoneLogin: false,
+      username: '',
+      password: '',
+      repassword: '',
+      smscode: '',
+      smsTime: 0,
     }
   },
   computed: {
     disabled() {
-      if (this.type === 'reg') {
-        return !this.form.username || !this.form.password || !this.form.repassword
+      if (this.formType === 'reg') {
+        return !this.username || !this.password || !this.repassword
       } else {
-        return !this.form.username || !this.form.password
+        if (this.phoneLogin) {
+          return !this.username || !this.smscode || !this.validate()
+        } else {
+          return !this.username || !this.password
+        }
       }
     },
   },
-  onLoad() {
-    const res = uni.getSystemInfoSync()
-    this.statusBarHeight = res.statusBarHeight
-  },
   methods: {
+    // 初始化表单
+    initForm() {
+      this.username = ''
+      this.password = ''
+      this.repassword = ''
+      this.smscode = ''
+    },
     // 切换表单
     changeType() {
-      this.type = this.type === 'login' ? 'reg' : 'login'
-      this.form = { username: '', password: '', repassword: '' }
+      this.phoneLogin = false
+      this.formType = this.formType === 'login' ? 'reg' : 'login'
+      this.initForm()
+    },
+    // 切换登入类型
+    changeLoginType() {
+      this.phoneLogin = !this.phoneLogin
+      this.initForm()
+    },
+    // 获取短信验证码
+    getSmscode() {
+      if (!this.validate()) {
+        return uni.showToast({
+          title: '手机号码格式不正确',
+          icon: 'none',
+        })
+      }
+      if (this.formType === 'login' && this.smsTime === 0) {
+        this.smsTime = 60
+        let timer = setInterval(() => {
+          if (this.smsTime > 0) {
+            this.smsTime--
+          } else {
+            clearInterval(timer)
+            this.smsTime = 0
+          }
+        }, 1000)
+      }
     },
     // 第三方登入
-    socialLogin(type) {
-      console.log(type)
+    socialLogin(e) {
+      console.log(e)
     },
     // 验证
-    check() {
-      if (this.type === 'reg') {
-        if (this.form.password !== this.form.repassword) {
+    validate() {
+      if (this.formType === 'reg') {
+        if (this.password !== this.repassword) {
           uni.showToast({
             title: '两次输入的密码不一致',
             icon: 'none',
@@ -153,29 +207,26 @@ export default {
           return false
         }
       } else {
+        // 手机格式验证
+        if (this.phoneLogin) return phoneRegex.test(this.username)
       }
       return true
     },
     // 注册||登入
     submit() {
-      if (!this.check()) return
-      let msg = this.type === 'reg' ? '注册' : '登入'
+      // 表单验证
+      if (!this.validate()) return
+      // 模拟提交后端
+      let msg = this.formType === 'reg' ? '注册' : '登入'
+      this.loading = true
+      setTimeout(() => {
+        this.loading = false
+      }, 2000)
+      // 成功处理
       uni.showToast({
         title: msg + '成功',
         icon: 'none',
       })
-      // this.$req.post('/' + this.type, this.form).then((res) => {
-      //   if (this.type === 'reg') {
-      //     this.changeType()
-      //   } else {
-      //     this.$store.dispatch('user/login', res)
-      //     this.back()
-      //   }
-      //   uni.showToast({
-      //     title: msg + '成功',
-      //     icon: 'none',
-      //   })
-      // })
     },
   },
 }
